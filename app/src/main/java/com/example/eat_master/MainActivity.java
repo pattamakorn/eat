b09b3da11a9511c871127318a268c0111f803ef5
KornPattamakorn <pattamakorn.s@hotmail.com>
2019-09-13 06:17:27 +0700

    13/09/2019 06:17

--- app/src/main/java/com/example/eat_master/MainActivity.java
+++ app/src/main/java/com/example/eat_master/MainActivity.java
@@ -7,8 +7,10 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
@@ -19,15 +21,22 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.favorite:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.kind_of_food:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
+                case R.id.wallet:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.recentapp:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+
             }
             return false;
         }
@@ -40,6 +49,30 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menutop, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem itemtop) {
+        int id = itemtop.getItemId();
+        switch (id) {
+            case R.id.setting_menu:
+                Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show();
+                //getSupportFragmentManager().beginTransaction().add(R.id.frameLayout,new qrcode()).commit();
+                return true;
+            case R.id.logout_menu:
+                Toast.makeText(this, "logout", Toast.LENGTH_SHORT).show();
+
+            case R.id.profile_menu:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+            default:
+                return super.onOptionsItemSelected(itemtop);
+        }
     }
 
 }
