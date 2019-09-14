997cdc6397ccb704f381afdcd9d279771b3f0a69
KornPattamakorn <pattamakorn.s@hotmail.com>
2019-09-15 03:04:23 +0700

    15/09/2019 03:04

--- app/src/main/java/com/example/eat_master/foodhitAdapter.java
+++ app/src/main/java/com/example/eat_master/foodhitAdapter.java
@@ -1,11 +1,14 @@
 package com.example.eat_master;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,6 +23,7 @@ public class foodhitAdapter extends RecyclerView.Adapter<foodhitAdapter.MyViewHo
     Context mcontext;
     List<footHit> mfoothit;
     View view;
+    Dialog myDialog;
     private ImageView img;
 
     public foodhitAdapter(Context mcontext, List<footHit> mfoothit) {
@@ -32,6 +36,14 @@ public class foodhitAdapter extends RecyclerView.Adapter<foodhitAdapter.MyViewHo
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         view = LayoutInflater.from(mcontext).inflate(R.layout.item_home,parent,false);
         MyViewHolder vHolder = new MyViewHolder(view);
+
+        vHolder.order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mcontext, "TestClick", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return vHolder;
     }
 
@@ -56,14 +68,17 @@ public class foodhitAdapter extends RecyclerView.Adapter<foodhitAdapter.MyViewHo
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
-        private TextView rnamefoot,namerest,price;
+        private LinearLayout item_click;
+        private TextView rnamefoot,namerest,price,order;
         public ImageView url_ima;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            item_click = itemView.findViewById(R.id.Mitem_home);
             rnamefoot = itemView.findViewById(R.id.namefood);
             namerest = itemView.findViewById(R.id.nameres);
             price = itemView.findViewById(R.id.pricefood);
             url_ima = itemView.findViewById(R.id.imagefood);
+            order = itemView.findViewById(R.id.orderFood);
 
 
         }
